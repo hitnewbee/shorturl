a2e13471e9c2b8e4d62e4768e118015a94e2f5d4
hitnewbee <17862700270@163.com>
2018-04-04 14:35:16 +0800

    system log filter

---
+++ app/Filters.java
@@ -0,0 +1,10 @@
+import play.http.DefaultHttpFilters;
+
+import javax.inject.Inject;
+
+public class Filters extends DefaultHttpFilters {
+    @Inject
+    public Filters(LoggingFilter loggingFilter){
+        super(loggingFilter);
+    }
+}
